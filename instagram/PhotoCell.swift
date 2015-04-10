04bc2ee63dc475f8e6764ea19181e5b9cf369577
Jim <caijim@github.com>
2015-04-09 20:27:06 -0700

    first commit

---
+++ instagram/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  instagram
+//
+//  Created by Jim Cai on 4/9/15.
+//  Copyright (c) 2015 com.codepath. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
